fbbca3ae4dba6d05d3b88e8d5ba812df93e4d105
Saurav Roy <sauravroy@Sauravs-Air-2.lan>
2026-06-06 13:59:38 +0530

    achieved data chuking

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from datasets import load_dataset
+
+def ragbenchloader(dataset_name: str, split: str = "test") -> None:
+    datasets = [
+        'covidqa', 'cuad', 'delucionqa',
+        'emanual', 'expertqa', 'finqa',
+        'hagrid', 'hotpotqa', 'msmarco',
+        'pubmedqa', 'tatqa', 'techqa'
+    ]
+
+    if dataset_name in datasets:
+        ragbench_dataset = load_dataset("rungalileo/ragbench", dataset_name, split=split)
+        print(type(ragbench_dataset))
+    else:
+        raise ValueError(f"Dataset '{dataset_name}' is not in the supported list.")
+
+
 def main():
     print("Hello from nextgen-rag-system!")
+    ragbenchloader("finqa")
 
 
 if __name__ == "__main__":
